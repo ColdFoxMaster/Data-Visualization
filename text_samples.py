4e5c55fb434aef243886e51a0885d465c8294f6e
nunopenim <nuno.penim@gmail.com>
2022-04-04 21:38:47 +0100

    general: Added a text sample

---
+++ text_samples.py
@@ -0,0 +1,7 @@
+GRAFICOS_NUNO = """
+Este textinho é um exemplo da informação que podemos dizer sobre o gráfico, uma vez que vamos fazer análises e 
+recomendações. Espero que a div que tenha feito funcione bem, visto que eu sinceramente detesto CSS e HTML, e não
+sei bem o que meter aqui nesta etapa do projeto. Ainda assim, está fixe se de facto funcionar bem. Dá para ver que
+o maior profit está a vir da tecnologia e office supplies, e não tanto dos móveis. Mas não importa para já, será
+algo a discutir posteriormente.
+"""
