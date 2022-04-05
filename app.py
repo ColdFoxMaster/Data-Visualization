67bf44be44d8f618cb0c71495e4bb14735cc7da0
nunopenim <nuno.penim@gmail.com>
2022-04-05 15:08:22 +0100

    general: added Gonçalo's map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
@@ -16,6 +17,89 @@ df = pd.read_csv('superstore.csv', encoding = "ISO-8859-1")
 
 df['Year'] = pd.to_datetime(df['Ship Date']).dt.year # added a year column for easier data sorting
 df.drop(df.index[df['Year'] == 2018], inplace = True) # 2018 has incomplete data, so we drop the whole year
+
+sub_categories = ['Bookcases', 'Chairs', 'Labels', 'Tables', 'Storage',
+       'Furnishings', 'Art', 'Phones', 'Binders', 'Appliances', 'Paper',
+       'Accessories', 'Envelopes', 'Fasteners', 'Supplies', 'Machines',
+       'Copiers']
+
+# State name to code
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+
+df['State'] = df['State'].map(lambda x: us_state_to_abbrev[x])
+
+# list of state codes
+
+states_codes = sorted(df['State'].unique())
+
+################################################RadioitemComponent#############################################################
+
+sub_categories_options = [dict(label=sub_category.replace('_', ' '), value=sub_category) for sub_category in sub_categories]
+
+radio_interaction = dcc.RadioItems(
+        id='interaction',
+        options=sub_categories_options,
+        value='Bookcases',
+        labelStyle={'display': 'block'}
+    )
 
 # Sales and Profit chart
 df_3bar = df.filter(['Sub-Category', 'Sales', 'Discount', 'Profit'], axis=1)
@@ -45,6 +129,18 @@ server = app.server
 
 app.layout = html.Div([
     html.H1(children='Projeto something'),
+
+    html.Div([
+        html.Div([
+            html.Label('Product Sub-Categories'),
+            radio_interaction
+        ], style={'width': "20%"}),
+
+        html.Div([
+            dcc.Graph(id='choropleth_graph')
+        ], style={'width': "80%"})
+    ], style={'display': 'flex'}),
+
     html.Div([
         html.Div([GRAFICOS_NUNO], style={'float': 'left', 'width': '19%'}),
         html.Div([
@@ -75,6 +171,50 @@ app.layout = html.Div([
 
     ])
 ])
+
+##############################################callback#############################################################################
+
+@app.callback(
+    Output('choropleth_graph', 'figure'),
+    Input('interaction', 'value')
+)
+
+##############################################Graph################################################################################
+
+
+# Building Choropleth Graph
+def plot(subgroup):
+
+    # I have to query the data to get only the sub_categories I want!!!!!
+
+    df_map = df.groupby(["Sub-Category", "State"]).sum("Quantity")["Quantity"]
+
+    d = dict.fromkeys(states_codes, 0)
+    z = dict(df_map[subgroup])
+
+    for key in z:
+        d[key] = z[key]
+    result = pd.Series(d)
+
+
+    data_choropleth = dict(type='choropleth',
+                           locations=states_codes,
+                           #There are three ways to 'merge' your data with the data pre embedded in the map
+                           locationmode='USA-states',
+                           z=result.astype(float),
+                           colorscale='Reds',
+                           colorbar=dict(title='Product Quantity')
+                          )
+
+    layout_choropleth = dict(geo=dict(scope='usa'),
+                             title=dict(text='United States Map of Purchases',
+                                        x=.5 # Title relative position according to the xaxis, range (0,1)
+                                       )
+                            )
+
+    fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
