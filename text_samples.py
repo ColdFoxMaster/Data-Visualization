5cb771bbc26b96daabebb9fe74b1077f124f55b1
nunopenim <nuno.penim@gmail.com>
2022-04-06 10:47:01 +0100

    general: toda a formatação pode agora ser feita em python, em vez de CSS porque ninguém merece sofrer com CSS

--- text_samples.py
+++ text_samples.py
@@ -1,7 +1,1 @@
-GRAFICOS_NUNO = """
-Este textinho é um exemplo da informação que podemos dizer sobre o gráfico, uma vez que vamos fazer análises e 
-recomendações. Espero que a div que tenha feito funcione bem, visto que eu sinceramente detesto CSS e HTML, e não
-sei bem o que meter aqui nesta etapa do projeto. Ainda assim, está fixe se de facto funcionar bem. Dá para ver que
-o maior profit está a vir da tecnologia e office supplies, e não tanto dos móveis. Mas não importa para já, será
-algo a discutir posteriormente.
-"""
+GRAFICOS_NUNO = "Este textinho é um exemplo da informação que podemos dizer sobre o gráfico, uma vez que vamos fazer análises e recomendações. Espero que a div que tenha feito funcione bem, visto que eu sinceramente detesto CSS e HTML, e não sei bem o que meter aqui nesta etapa do projeto. Ainda assim, está fixe se de facto funcionar bem. Dá para ver que o maior profit está a vir da tecnologia e office supplies, e não tanto dos móveis. Mas não importa para já, será algo a discutir posteriormente."
